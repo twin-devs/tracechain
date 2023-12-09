b86ed706004f243388b79491c6c70b3c54a1f8e5
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-12-09 23:35:04 +0530

    add connectors and hooks needed (#13)

---
+++ traceui/hooks/index.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+export const useHasMounted = () => {
+    const [hasMounted, setHasMounted] = useState(false);
+  
+    useEffect(() => {
+      setHasMounted(true);
+    }, []);
+  
+    return hasMounted;
+  };
--- traceui/pages/index.tsx
+++ traceui/pages/index.tsx
@@ -2,8 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Box } from "@obolnetwork/obol-ui";
 import { DEFAULT_NETWORK_NAME } from "../constants";
-import { SelectForm } from "@components/organisms/select-form";
-
 const Home: NextPage = () => {
   const networkName = DEFAULT_NETWORK_NAME.toLocaleLowerCase();
   return (
@@ -33,7 +31,6 @@ const Home: NextPage = () => {
           mb: "$4xl",
         }}
       >
-      <SelectForm />
       </Box>
     </div>
   );
