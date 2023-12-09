1085f2c161b5c21e526f3a2ee5cdc927eb0b6d73
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-12-09 20:14:43 +0530

    metamask integration with smart contracts (#10)

--- traceui/src/app/mfgreg.js
+++ traceui/src/app/mfgreg.js
@@ -1,15 +1,22 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 
 const trackingABI = `[{"inputs":[{"internalType":"string","name":"upcCode","type":"string"}],"name":"attest","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"upcCode","type":"string"}],"name":"getHistory","outputs":[{"components":[{"internalType":"address","name":"attester","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"struct Tracking.att[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"upcCode","type":"string"}],"name":"getMetadata","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"partyEthAddress","type":"address"}],"name":"getPartyDetails","outputs":[{"components":[{"internalType":"string","name":"partyID","type":"string"},{"internalType":"string","name":"registeredAddress","type":"string"},{"internalType":"string","name":"registrationNo","type":"string"},{"internalType":"string","name":"GSTNumber","type":"string"}],"internalType":"struct Tracking.partyDetails","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"upcCode","type":"string"},{"internalType":"string","name":"contentID","type":"string"}],"name":"registerItem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"partyID","type":"string"},{"internalType":"string","name":"registeredAddress","type":"string"},{"internalType":"string","name":"registrationNo","type":"string"},{"internalType":"string","name":"GSTNumber","type":"string"}],"name":"registerParty","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
 
 // Address of deployed tracking contract.
-const addr = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
-const provider = new ethers.BrowserProvider(window.ethereum);
-const track = new ethers.Contract(addr, trackingABI, provider);
+const chainIdToContracts = {
+    "421614": "0x7e64463DebD272F12AD61dc991808aB209841633", // Arbitrum Sepolia
+    "84531": "0x7e64463DebD272F12AD61dc991808aB209841633", // Base Sepolia
+    "5001": "0x7e64463DebD272F12AD61dc991808aB209841633", // Mantle testnet
+    "534351": "0x88F5804fe2731abe4A94D08136E0445006748BA1", // Scroll Sepolia
+    "1442": "0xeBB9868674Daa23A54e5235e950D78a733a00393", // Polygon ZkEVM testnet
+    "44787": "0x88F5804fe2731abe4A94D08136E0445006748BA1", // Celo Alfajores testnet
+    "7001": "0x7e64463DebD272F12AD61dc991808aB209841633", // Zetachain Athen-3
+    "195": "0x7e64463DebD272F12AD61dc991808aB209841633", // OKX X1 Testnet
+}
 
 const MfgReg = () => {
     const [fieldValues, setFieldValues] = useState({
@@ -25,8 +32,13 @@ const MfgReg = () => {
 
     // Step 1: Each party (manufacturer/intermediary) registers itself with the contract.
     const registerParty = async (registeredAddress, registrationNo, GSTNumber) => {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const chainId = (await provider.getNetwork()).chainId.toString();
+        const track = new ethers.Contract(chainIdToContracts[chainId], trackingABI, provider);
+        const signer = await provider.getSigner();
+
         const partyID = uuidv4();
-        const tx = await track.connect(window.ethereum).registerParty(
+        const tx = await track.connect(signer).registerParty(
             partyID,
             registeredAddress,
             registrationNo,
@@ -35,6 +47,9 @@ const MfgReg = () => {
         await tx.wait();
 
         console.log("Party registered", tx.hash);
+
+        const details = await track.connect(provider).getPartyDetails(signer.address);
+        console.log("party details from contract: ", details);
     }
 
     const handleReg = async () => {
