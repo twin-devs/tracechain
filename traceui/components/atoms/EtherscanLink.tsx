65d9eb8a67e8d5a3d32ccbb23337a372cacec8b6
Dhruv Bodani <dhruvbodani2510@gmail.com>
2023-12-10 07:26:58 +0530

    upload encrypted data (#20)

--- traceui/components/atoms/EtherscanLink.tsx
+++ traceui/components/atoms/EtherscanLink.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Flex, Link, Text } from "@obolnetwork/obol-ui";
 import { ellipseAddress } from "../../utils/eth";
+import { chainIdToExplorer } from "@constants/index";
 
-const EtherscanLink = ({ txHash }) => {
-    const etherscanBaseUrl = `${process.env.NEXT_PUBLIC_ETHERSCAN}/tx`; // Base URL for Etherscan transaction pages
+const EtherscanLink = ({ txHash, chainId }) => {
+    const etherscanBaseUrl = `${chainIdToExplorer[chainId]}/tx`; // Base URL for Etherscan transaction pages
 
     return (
         <Flex css={{ alignItems: "center", gap: "$xxxs" }}>
